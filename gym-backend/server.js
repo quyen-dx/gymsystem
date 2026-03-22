c733afd50cbfa9f94da1e1f121918312e5d48052
quyen-dx <daoxuanquyen333@gmail.com>
2026-03-22 16:21:10 +0700

    feat: initial project setup

---
+++ gym-backend/server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect('mongodb://127.0.0.1:27017/gym')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err))
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000')
+})
