b90467e9b1490e0d3eda20b571da60bd3fb88326
quyen-dx <daoxuanquyen333@gmail.com>
2026-03-23 10:31:25 +0700

    ahi

--- gym-frontend/src/App.tsx
+++ gym-frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-red-500 font-bold text-9xl'>Ahihi</h1>
+      <h1 className='text-red-500 font-bold text-9xl'>Ahihjjsjvksjki</h1>
     </>
   )
 }
