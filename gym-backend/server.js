65feda89ce3583cf8d728fb12e0892d5ed2d63be
quyen-dx <daoxuanquyen333@gmail.com>
2026-03-24 22:45:18 +0700

    update server

--- gym-backend/server.js
+++ gym-backend/server.js
@@ -1,16 +1,17 @@
+import cors from 'cors'
 import express from 'express'
 import mongoose from 'mongoose'
-import cors from 'cors'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express()
-
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('mongodb://127.0.0.1:27017/gym')
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err))
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ MongoDB connected'))
+  .catch(err => console.log('❌ DB Error:',err))
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000')
+  console.log(`🚀 Server running on port ${process.env.PORT}`)
 })
